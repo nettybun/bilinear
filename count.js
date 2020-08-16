40170b44eec35cd1c2fb6b8d3eb2ff44962a1966
Grant Hames-Morgan <hamesg@tutanota.de>
2020-08-15 22:45:26 -0700

    CSV of ASM instruction analysis

--- count.js
+++ count.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
 const files = process.argv.slice(2);
+// For CSV
+console.log(`file,content.length,instruction,count,percentage`);
 files.forEach(work);
 
 function work(file) {
   if (!file) throw 'No file';
   const content = fs.readFileSync(file, 'utf-8').split('\n');
-  console.log(`File "${file}"`)
-  console.log(`${content.length} lines`);
+  // For non-CSV
+  // console.log(`File "${file}"`)
+  // console.log(`${content.length} lines`);
 
   const count = new Map();
   for (const line of content) {
@@ -25,8 +28,13 @@ function work(file) {
   const total = arr.reduce((acc, o) => acc += o.count, 0);
   for (const { name, count } of arr) {
     const percentage = (count / total) * 100;
-    if (percentage < 1) break;
-    console.log(`${name.padEnd(15)}${String(count).padEnd(5)} ${percentage.toFixed(2)}%`);
+    // For CSV
+    console.log(`${file},${content.length},${name},${count},${percentage.toFixed(2)}%`);
+
+    // For non-CSV
+    // if (percentage < 1) break;
+    // console.log(`${name.padEnd(15)}${String(count).padEnd(5)} ${percentage.toFixed(2)}%`);
+
     // For the report (Google Docs merges spaces so need to use periods...)
     // console.log(`${(name + ' ').padEnd(15, '.')} ${(count + ' ').padEnd(5, '.')} ${percentage.toFixed(2)}%`);
   }
