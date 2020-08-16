e70bb18f8c6e8cd0a2aecf903cae520fef42522a
Grant Hames-Morgan <hamesg@tutanota.de>
2020-08-15 18:36:25 -0700

    Count ASM

---
+++ count.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const files = process.argv.slice(2);
+files.forEach(work);
+
+function work(file) {
+  if (!file) throw 'No file';
+  const content = fs.readFileSync(file, 'utf-8').split('\n');
+  console.log(`File "${file}"`)
+  console.log(`${content.length} lines`);
+
+  const count = new Map();
+  for (const line of content) {
+    if (line[0] !== '\t' || line[1] === '.') {
+      // console.log('Skip', line);
+      continue;
+    }
+    const split = line.trim().split(/\s+/);
+    const match = split[0];
+    const res = (count.get(match) || { name: match, count: 0 });
+    res.count++;
+    count.set(match, res);
+  }
+
+  const arr = Array.from(count.values()).sort((a, b) => b.count - a.count);
+  const total = arr.reduce((acc, o) => acc += o.count, 0);
+  for (const { name, count } of arr) {
+    const percentage = (count / total) * 100;
+    if (percentage < 1) break;
+    console.log(`${name.padEnd(15)}${String(count).padEnd(5)} ${percentage.toFixed(2)}%`);
+    // For the report (Google Docs merges spaces so need to use periods...)
+    // console.log(`${(name + ' ').padEnd(15, '.')} ${(count + ' ').padEnd(5, '.')} ${percentage.toFixed(2)}%`);
+  }
+  console.log('\n');
+}
