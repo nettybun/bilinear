a6394c6f23bdb2f5c5544c052058430aa5318fbd
Grant Hames-Morgan <hamesg@tutanota.de>
2020-08-16 16:24:54 -0700

    Build script fix

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 
 set -x
 mkdir -p bin
-gcc -g -gddb3 bmp_to_simple_convert.c bmp.c simple.c -o bin/bmp_to_simple_convert
-gcc -g -gddb3 simple_to_bmp_convert.c bmp.c simple.c -o bin/simple_to_bmp_convert
-gcc -g -gddb3 scale.c simple.c -o bin/scale
+gcc -g bmp_to_simple_convert.c bmp.c simple.c -o bin/bmp_to_simple_convert
+gcc -g simple_to_bmp_convert.c bmp.c simple.c -o bin/simple_to_bmp_convert
+#gcc -g scale.c simple.c -o bin/scale
+arm-linux-gnueabi-gcc -g -march=armv7-a -mfloat-abi=softfp -mfpu=neon scale.c simple.c -o bin/scale
 
-#arm-linux-gnueabi-gcc -march=armv7-a -mfloat-abi=softfp -mfpu=neon -S scale.c
+# Also generate the ASM
+arm-linux-gnueabi-gcc -march=armv7-a -mfloat-abi=softfp -mfpu=neon -S scale.c
